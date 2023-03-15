30dd7ddd7dde641beb57a49cb8a9d8e450737afd
guoxiaowei <597509924@qq.com>
2023-03-15 18:46:29 +0800

    first commit

---
+++ src/main/java/com/gxw/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.gxw.git;
+
+/**
+ * @title: GitTest
+ * @author: GXW
+ * @date: 2023/3/15 18:43
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
