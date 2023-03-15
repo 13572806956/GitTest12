622b510f24ab5708414ac0a0c5647e87c4c48c2b
guoxiaowei <597509924@qq.com>
2023-03-15 18:51:31 +0800

    second commit

--- src/main/java/com/gxw/git/GitTest.java
+++ src/main/java/com/gxw/git/GitTest.java
@@ -8,5 +8,6 @@ package com.gxw.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Hello Git2");
     }
 }
